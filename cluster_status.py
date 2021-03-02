c43dff8e158aab88c4bb10789b3de8ebe82e7cf5
betorre <betorre@gmail.com>
2021-03-02 12:55:44 -0500

    UPDATE #2 12:54 02032021 Added 2 more functions to extract H1 and H2 from the same excel file, also based on user input of NSID

--- cluster_status.py
+++ cluster_status.py
@@ -12,11 +12,14 @@ import xlrd
 
 row = 2
 column = 4
-row_x = 0
-column_x = 4
+row_nsid = 0
+column_vip = 4
+column_h1 = 5
+column_h2 = 6
 
 #FUNCTION DEFINITION
-def get_ns_vip(row_x,column_x):
+###GET NS VIP based on user input of NS ID
+def get_ns_vip(row_nsid,column_vip):
 
     #READ LIST OF NETWORK SEGMENTS
     #Read Excel table with NS VLAN 18 tables
@@ -30,17 +33,51 @@ def get_ns_vip(row_x,column_x):
     sheet = wb.sheet_by_index(0)
     
     # For row 0 and column 0
-    print(sheet.cell_value(row_x, column_x))
+    print(sheet.cell_value(row_nsid,column_vip))
+###
+###GET H1-NSCX IP based on User Input of NSID
+def get_ns_h1(row_nsid,column_h1):
 
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: 
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_h1))
+###
+###GET H2-NSCX IP based on User Input of NSID
+def get_ns_h2(row_nsid,column_h2):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: 
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_h2))
+###
 #MAIN LOOP5
 
 NS = int(input("ENTER NETWORK SEGMENT: "))
 print("SELECTED NETWORK SEGMENT IS ",NS)
 #calculate NS ROWS and COLUMNS by adding NS to start variables "row" and "column" and storing the result on variables "row_x" and "column_x"
-row_x = row + NS
+row_nsid = row + NS
 
 #CALL get_ns_vip() with calculated rows and columns
-get_ns_vip(row_x,column_x)
+get_ns_vip(row_nsid,column_vip)
 
 
 
