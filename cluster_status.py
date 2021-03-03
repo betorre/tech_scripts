f95613b2343c20d4350b13515594c9953db716db
betorre <betorre@gmail.com>
2021-03-02 21:33:38 -0500

    UPDATE #4 21:32 02032021 "Added additional information to source table, and created functions to fetch DPS, HSP, MCR100 to 103 and IPM according to User input for NSID""

--- cluster_status.py
+++ cluster_status.py
@@ -16,6 +16,13 @@ row_nsid = 0
 column_vip = 4
 column_h1 = 5
 column_h2 = 6
+column_dps = 12
+column_hsp = 13
+column_mcr100 = 14
+column_mcr101 = 15
+column_mcr102 = 16
+column_mcr103 = 17
+column_ipm = 18
 
 #FUNCTION DEFINITION
 ###GET NS VIP based on user input of NS ID
@@ -59,7 +66,7 @@ def get_ns_h2(row_nsid,column_h2):
     #Read Excel table with NS VLAN 18 tables
 
     #Location of Scripts/Support files "C:\CODE\NS.xls"
-    #NOTE: 
+    #NOTE: double backslash is needed for unicode/utf8 interaction
     loc = ('C:\\CODE\\NS.xls')
     
     # To open Workbook
@@ -69,7 +76,126 @@ def get_ns_h2(row_nsid,column_h2):
     # For row 0 and column 0
     print(sheet.cell_value(row_nsid, column_h2))
 ###
-#MAIN LOOP5
+###GET DPS IP based on User Input of NSID
+def get_ns_dps(row_nsid,column_dps):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_dps))
+###
+###GET HSP IP based on User Input of NSID
+def get_ns_hsp(row_nsid,column_hsp):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_hsp))
+###
+###GET MCR100 IP based on User Input of NSID
+def get_ns_mcr100(row_nsid,column_mcr100):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_mcr100))
+###
+###GET MCR101 IP based on User Input of NSID
+def get_ns_mcr101(row_nsid,column_mcr101):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_mcr101))
+###
+###GET MCR102 IP based on User Input of NSID
+def get_ns_mcr102(row_nsid,column_mcr102):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_mcr102))
+###
+###GET MCR103 IP based on User Input of NSID
+def get_ns_mcr103(row_nsid,column_mcr103):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_mcr103))
+###
+###GET IPM IP based on User Input of NSID
+def get_ns_ipm(row_nsid,column_ipm):
+
+    #READ LIST OF NETWORK SEGMENTS
+    #Read Excel table with NS VLAN 18 tables
+
+    #Location of Scripts/Support files "C:\CODE\NS.xls"
+    #NOTE: double backslash is needed for unicode/utf8 interaction
+    loc = ('C:\\CODE\\NS.xls')
+    
+    # To open Workbook
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    # For row 0 and column 0
+    print(sheet.cell_value(row_nsid, column_ipm))
+###
+#MAIN LOOP
 
 NS = int(input("ENTER NETWORK SEGMENT: "))
 print("SELECTED NETWORK SEGMENT IS ",NS)
@@ -80,6 +206,13 @@ row_nsid = row + NS
 get_ns_vip(row_nsid,column_vip)
 get_ns_h1(row_nsid,column_h1)
 get_ns_h2(row_nsid,column_h2)
+get_ns_dps(row_nsid,column_dps)
+get_ns_hsp(row_nsid,column_hsp)
+get_ns_mcr100(row_nsid,column_mcr100)
+get_ns_mcr101(row_nsid,column_mcr101)
+get_ns_mcr102(row_nsid,column_mcr102)
+get_ns_mcr103(row_nsid,column_mcr103)
+get_ns_ipm(row_nsid,column_ipm)
 
 
 
